5c963404970d7bbb301b97461def0df5191c2a92
somnathb583 <62545534+somnathb583@users.noreply.github.com>
2023-07-09 15:33:02 +0000

    added

--- script.js
+++ script.js
@@ -3,17 +3,31 @@ let isLoggedIn = false;
 const loggedComponent = document.querySelector(".login-signup");
 const loggedComponentHeader = document.querySelector(".heading");
 const BodyContent = document.querySelector(".content");
-
+const logginForm = document.getElementById("loggin-form");
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 const submitClickEvent = document.getElementById('button');
 
 submitClickEvent.addEventListener('click', () => {
-    console.log("clicked");
-    // console.log(username.value);
-    fetch('')
-        .then(response => response.json())
-        .then(data => console.log(data));
+    console.log(username.value + password.value);
+    event.preventDefault();
+    logginForm.reset();
+
+    fetch('https://test-notepad.onrender.com/UserApplication/user-login?skipAuth=true', {
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json',
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept'
+        },
+        body : JSON.stringify({
+            email : username.value,
+            userpassword : username.value
+        })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
 })
 
 if (!isLoggedIn) {
